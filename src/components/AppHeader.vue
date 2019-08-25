0213c8bc5eccaf28c47169195e8d09ff7c6403f9
sinouhe <absolom2001@hotmail.com>
2019-08-25 23:52:16 +0200

    en cours

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="" class="active item">
+            Image Storage
+        </a>
+        {{ isLoggedIn }}
+        <div class="right menu">
+            <a href="#" class="ui item" @click="login">
+                login
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    name: 'AppHeader',
+    computed: mapGetters(['isLoggedIn']),
+    methods: mapActions(['login'])
+}
+</script>
